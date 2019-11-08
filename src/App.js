16b050d1af7760edd77bbd00e2857e0bd2fb7945
Spriha <spriha@mindfiresolutions.com>
2019-11-08 19:08:07 +0530

    added functionality to make the selected text bold

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ import {
 
 class App extends Component {
   state = {
+    totalValue: "",
+    selectValue: "",
     list: [
       {
         label: faBold,
@@ -32,21 +34,62 @@ class App extends Component {
     super(props);
     this.textAreaRef = React.createRef();
     this.getSelection = this.getSelection.bind(this);
+    this.handleEvent = this.handleEvent.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleCaptureChange = this.handleCaptureChange.bind(this);
   }
 
   getSelection = () => {
     const textArea = this.textAreaRef.current;
+    const totalValue = textArea.value;
+    console.log(textArea.selectionStart);
+    console.log(textArea.selectionEnd);
+
+    const selectValue = textArea.value.substring(
+      textArea.selectionStart,
+      textArea.selectionEnd
+    );
+    this.setState({ totalValue, selectValue });
     console.log(
       textArea.value.substring(textArea.selectionStart, textArea.selectionEnd)
     );
+  };
+
+  handleEvent = () => {
+    const totalValue = this.state.totalValue;
+    const oldValue = this.state.selectValue;
+    const newValue = "<strong>" + this.state.selectValue + "</strong>";
+    const result = totalValue.replace(oldValue, newValue);
+    console.log("total", totalValue);
+    console.log("old", oldValue);
+    // console.log("new", dangerouslySetInnerHTML(newValue));
+    console.log(result);
+  };
+
+  handleChange = () => {
+    consile.log("change");
+  };
+
+  handleCaptureChange = () => {
+    consile.log("capture");
   };
 
   render() {
     const { list } = this.state;
     return (
       <div className="card mx-auto my-auto">
+        <div
+          contentEditable="true"
+          onChangeCapture={() => this.handleCaptureChange()}
+          onChange={() => this.handleChange()}
+        >
+          hello
+        </div>
         <div className="card-header">
           <ul className="list-group list-group-horizontal">
+            <li>
+              <button onClick={() => this.handleEvent()}>Event</button>
+            </li>
             {list.map(listItem => (
               <ListItem
                 key={listItem.id}
