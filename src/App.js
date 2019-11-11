a9665009a9e05c92704b6fa646c05ee18b67922e
Spriha <spriha@mindfiresolutions.com>
2019-11-11 12:58:08 +0530

    added feature to make selected text bold or italic

--- src/App.js
+++ src/App.js
@@ -1,113 +1,33 @@
-import React, { Component } from "react";
-import { hot } from "react-hot-loader";
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.css";
-import ListItem from "./components/listItem";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faBold,
-  faItalic,
-  faUnderline
-} from "@fortawesome/free-solid-svg-icons";
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import { Editor, EditorState, RichUtils } from "draft-js";
 
-class App extends Component {
-  state = {
-    totalValue: "",
-    selectValue: "",
-    list: [
-      {
-        label: faBold,
-        id: 1
-      },
-      {
-        label: faItalic,
-        id: 2
-      },
-      {
-        label: faUnderline,
-        id: 3
-      }
-    ]
-  };
+function MyEditor() {
+  const [editorState, setEditorState] = useState(EditorState.createEmpty());
 
-  constructor(props) {
-    super(props);
-    this.textAreaRef = React.createRef();
-    this.getSelection = this.getSelection.bind(this);
-    this.handleEvent = this.handleEvent.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-    this.handleCaptureChange = this.handleCaptureChange.bind(this);
+  // handleChange = editorState => setEditorState(editorState);
+  // function handleChange(editorState) {
+  //   setEditorState(editorState);
+  // }
+
+  function handleKeyCommand(command, editorState) {
+    const newState = RichUtils.handleKeyCommand(editorState, command);
+    if (newState) {
+      this.onChange(newState);
+      return "handled";
+    }
+    return "not-handled";
   }
 
-  getSelection = () => {
-    const textArea = this.textAreaRef.current;
-    const totalValue = textArea.value;
-    console.log(textArea.selectionStart);
-    console.log(textArea.selectionEnd);
-
-    const selectValue = textArea.value.substring(
-      textArea.selectionStart,
-      textArea.selectionEnd
-    );
-    this.setState({ totalValue, selectValue });
-    console.log(
-      textArea.value.substring(textArea.selectionStart, textArea.selectionEnd)
-    );
-  };
-
-  handleEvent = () => {
-    const totalValue = this.state.totalValue;
-    const oldValue = this.state.selectValue;
-    const newValue = "<strong>" + this.state.selectValue + "</strong>";
-    const result = totalValue.replace(oldValue, newValue);
-    console.log("total", totalValue);
-    console.log("old", oldValue);
-    // console.log("new", dangerouslySetInnerHTML(newValue));
-    console.log(result);
-  };
-
-  handleChange = () => {
-    consile.log("change");
-  };
-
-  handleCaptureChange = () => {
-    consile.log("capture");
-  };
-
-  render() {
-    const { list } = this.state;
-    return (
-      <div className="card mx-auto my-auto">
-        <div
-          contentEditable="true"
-          onChangeCapture={() => this.handleCaptureChange()}
-          onChange={() => this.handleChange()}
-        >
-          hello
-        </div>
-        <div className="card-header">
-          <ul className="list-group list-group-horizontal">
-            <li>
-              <button onClick={() => this.handleEvent()}>Event</button>
-            </li>
-            {list.map(listItem => (
-              <ListItem
-                key={listItem.id}
-                id={listItem.id}
-                label={listItem.label}
-              />
-            ))}
-          </ul>
-        </div>
-        <div className="card-body">
-          <textarea
-            onMouseUp={() => this.getSelection()}
-            ref={this.textAreaRef}
-          ></textarea>
-        </div>
-      </div>
-    );
-  }
+  return (
+    <Editor
+      editorState={editorState}
+      handleKeyCommand={handleKeyCommand}
+      onChange={setEditorState}
+    />
+  );
 }
 
-export default hot(module)(App);
+export default MyEditor;
+
+// ReactDOM.render(<MyEditor />, document.getElementById("root"));
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App.js";
+import MyEditor from "./App.js";
+import "../node_modules/draft-js/dist/Draft.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<MyEditor />, document.getElementById("root"));
 // export default App;
